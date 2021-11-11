42948fd53a782bbb2cc7324ec2dab213774eca06
Someone Rude <example@mail.com>
2021-11-11 13:22:53 +0300

    fixing class and adding collisions

--- solar_objects.py
+++ solar_objects.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 # license: GPLv3
 
+gravitational_constant = 6.67408E-11
 
 class Body:
     """Тип данных, описывающий звезду.
@@ -35,4 +36,61 @@ class Body:
         self.color = "red"
         """Цвет звезды"""
 
-    
+    def move(self, dt):
+        '''Меняет скорость и координату за малый промежуток dt'''
+
+        self.vy += self.fy / self.m * dt
+        self.vx += self.fx / self.m * dt
+        self.y += self.vy * dt
+        self.x += self.vx * dt
+
+    def draw(self, surface):
+        '''
+        Отрисовывает тело на поверхности
+        surface - поверхность для отрисовки
+        self.x , self.y - координаты тела
+        self.r - радиус тела
+        self. color - цвет тела
+        '''
+        pg.circle(surface, self.color, (self.x, self.y) , self.r)
+
+    def distance_check(self, x , y , rad):
+        '''
+        Считает расстояние от тела до тела
+        '''
+        distance_q = (x - self.x) ** 2 + (y - self.y) ** 2
+        if distance_q < (self.r + rad) ** 2:
+            return False
+        else:
+            return True
+
+    def field(self , x , y):
+        '''
+        Считает поле в заданной точке
+        x, y - координаты точки
+        '''
+        distance_q = (x - self.x) ** 2 + (y - self.y) ** 2
+        electric_field = gravitational_constant * self.m / distance_quadratic
+        return electric_field
+
+
+def collision_check(body_1 , body_2):
+    return not body_1.distance_check(body_2.x , body_2.y , body_2.r)
+
+
+def collision(body_1, body_2):
+    body_new = Body()
+    body_new.m = body_1.m + body_2.m
+    body_new.r = (body_1.r ** 3 + body_2.r ** 3) ** (1/3)
+    body_new.vx = (body_1.vx * body_1.m + body_2.vx * body_2.m) / body_new.m
+    body_new.vy = (body_1.vy * body_1.m + body_2.vy * body_2.m) / body_new.m
+    body_new.x = (body_1.x * body_1.m + body_2.m * body_2.x) / body_new.m
+    body_new.y = (body_1.y * body_1.m + body_2.m * body_2.y) / body_new.m
+    body_new.color = choice(body_1.color , body_2.color)
+    body_new.fx = body_1.fx + body_2.fx
+    body_new.fy = body_1.fy + body_2.fy
+    return body_new
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
